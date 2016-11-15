1622e39078bb64d18f1e0c519f8d650e5d0583d2
rasso92 <rasso92@hotmail.com>
2016-11-15 00:46:59 -0600

    Tarea 2
    Tabla de multiplicar del  7 - Table View

---
+++ Tarea2/Tarea2/TableViewDefaultExample/MathTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  DogsTableViewController.h
+//  TableViewDefaultExample
+//
+//  Created by Estudiantes on 29/10/16.
+//  Copyright © 2016 Cenfotec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MathTableViewController : UITableViewController
+@property(nonatomic,strong) NSMutableArray *operationsArray;
+@property(nonatomic,strong) NSMutableArray *resultArray;
+
+@end
